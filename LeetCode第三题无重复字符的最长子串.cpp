e2961e3f2735bb3525d99b0586c89cfa44f9d098
振森 刘 <1570132984@qq.com>
2021-10-12 16:32:24 +0800

    移动窗口

--- LeetCode第三题无重复字符的最长子串.cpp
+++ LeetCode第三题无重复字符的最长子串.cpp
@@ -2,12 +2,37 @@
 #include<string>
 #include<unordered_map>
 #include<vector>
+#include<unordered_set>
 using namespace std;
 
 class lengthofLongestSubstring 
 {
 public:
-    //方法一：暴力解法，时间复杂度为O(n*2)
+    //官方解法，移动窗口
+    int lengthOfLongestSubstring(string s) {
+        // 哈希集合，记录每个字符是否出现过
+        unordered_set<char> occ;
+        int n = s.size();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        // 枚举左指针的位置，初始值隐性地表示为 -1
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.erase(s[i - 1]);
+            }
+            while (rk + 1 < n && !occ.count(s[rk + 1])) {
+                // 不断地移动右指针
+                occ.insert(s[rk + 1]);
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+
+    //方法一：
     int BaoLi1(string& s)
     {
         int start(0), end(0), length(0), result(0);
@@ -83,12 +108,13 @@ public:
 
 int main()
 {
-    string s = "abcabd";
+    string s = "abcabde";
     lengthofLongestSubstring test;
     int RESULT = 0;
     //RESULT = test.BaoLi1(s);
     //RESULT = test.HashMap2(s);
-    RESULT = test.Vector3(s);
+   // RESULT = test.Vector3(s);
+    RESULT = test.lengthOfLongestSubstring(s);
     cout << "输入字符串s = " << s << " 的无重复字符子串的最大长度为：" << RESULT << endl; 
     
     system("pause");
